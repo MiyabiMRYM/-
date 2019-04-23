7a3315aebacd184ec773050a9a771b6b398d2d7e
MiyabiMRYM <kono.miyabi@ymobile.ne.jp>
2019-04-23 17:50:22 +0900

    はじめてのアプリ　フォント３０にした

---
+++ FirstKadaiApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirstKadaiApp
+//
+//  Created by Miyabi Muroyama on 2019/04/23.
+//  Copyright © 2019 Miyabi Muroyama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
